4e90669363c7898d30708463f9a9b88ef3de050d
Ronnel <omboyronnel@gmail.com>
2021-10-12 18:55:21 +0800

    change

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn MyDbProject.wsgi
--- myapp1/registrationview.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import View
-
-
-# Create your views here.
-
-class RegistrationView(View):
-    def get(self, request):
-            return render(request,'registration.html',{})
